f0d7ffab5d01ee93d411b784adda0689d55a5513
Rokas <Rokas.Imelinskas@gmail.com>
2025-02-12 14:59:30 +0200

    sync

---
+++ bandymai/02-12_uzduotys.js
@@ -0,0 +1,176 @@
+// 1. Parašykite funkciją, kuri priima skaičių kaip argumentą ir grąžina jo kvadratą.
+// (Pvz., kvadratu(4) turi grąžinti 16.)
+
+function kvadratas(num) {
+  return (num = num * num);
+}
+
+console.log("     Pirma");
+console.log(kvadratas(4));
+
+// 2. Parašykite funkciją, kuri patikrina, ar perduotas skaičius yra lyginis.
+// (Pvz., arLyginis(8) → true, arLyginis(7) → false.)
+
+function arlLyginis(num1) {
+  if (num1 % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log("     Antra");
+console.log(arlLyginis(8));
+console.log(arlLyginis(7));
+
+// 3. Sukurkite funkciją, kuri priima du skaičius ir grąžina didesnįjį.
+// (Pvz., didesnisSkaicius(5, 8) → 8.)
+
+function didesnisSkaicius(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+console.log("     Trečia");
+console.log(didesnisSkaicius(5, 8));
+
+// 4. Sukurkite funkciją, kuri priima stringą ir grąžina jį atvirkštine tvarka.
+// (Pvz., reverseString("JavaScript") → "tpircSavaJ".)
+
+const ava = "JavaScript";
+function reverseString(ava) {
+  return ava.split("").reverse().join("");
+}
+
+console.log("     Ketvirta");
+console.log(reverseString(ava));
+
+// 5. Parašykite funkciją, kuri skaičiuoja, kiek kartų nurodyta raidė pasikartoja string'e.
+// (Pvz., skaiciuotiRaides("banana", "a") → 3.)
+
+let countA = 0;
+//const word = "banana";
+
+function skaiciuotiRaides(a, b) {
+  for (i = 0; i < a.length; i++) {
+    if (a[i] === "a") {
+      countA++;
+    }
+  }
+  return countA;
+}
+
+console.log("     Penkta");
+//console.log(countA);
+console.log(skaiciuotiRaides("banana", "a"));
+
+// 6. Sukurkite funkciją, kuri priima skaičių ir grąžina "teigiamas", "neigiamas" arba "nulis" naudojant ternary.
+function teigiamas(a) {
+  return a > 0 ? "teigiamas" : a < 0 ? "neigiamas" : "nulis";
+}
+
+console.log("     Šešta");
+console.log(teigiamas(2));
+console.log(teigiamas(-2));
+console.log(teigiamas(0));
+
+// 7. Parašykite funkciją, kuri tikrina, ar žodis prasideda raide "A"(nesvarbu, didžioji ar mažoji).
+// (Pvz., pradedaA("Automobilis") → true, pradedaA("namas") → false.)
+
+function prasidedaA(zodis1) {
+  return zodis1.toLowerCase().charAt(0) === "a";
+}
+
+console.log("     Septinta");
+console.log(prasidedaA("Automobilis"));
+console.log(prasidedaA("namas"));
+
+// 8. Sukurkite funkciją, kuri priima skaičių ir patikrina, ar jis dalus iš 3 ir 5(grąžina true arba false).
+function dalus(a) {
+  return a % 3 === 0 && a % 5 === 0 ? true : false;
+}
+
+console.log("     Aštunta");
+console.log(dalus(15));
+console.log(dalus(5));
+
+// 9. Naudodami for ciklą, išveskite visus skaičius nuo 10 iki 1 mažėjančia tvarka.
+
+console.log("     Devinta");
+
+for (i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+// 10. Parašykite for ciklą, kuris išveda tik lyginius skaičius nuo 2 iki 20.
+
+console.log("     Dešimta");
+for (i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+// 11. Naudodami for ciklą, suskaičiuokite visų skaičių nuo 1 iki 100 sumą ir grąžinkite rezultatą.
+let rezul = 0;
+for (i = 1; i <= 100; i++) {
+  rezul = rezul + i;
+}
+
+console.log("     Vienuolikta");
+console.log(rezul);
+
+// 12. Sukurkite funkciją, kuri priima masyvą skaičių ir grąžina jų vidurkį.
+// (Pvz., vidurkis([5, 10, 15]) → 10.)
+
+let average = 0;
+function vidurkis(a) {
+  for (i = 0; i < a.length; i++) {
+    average = average + a[i];
+  }
+  return average / a.length;
+}
+console.log("     Dvylikta");
+console.log(vidurkis([5, 10, 15]));
+
+// 13. Naudodami for ciklą, raskite didžiausią skaičių iš pateikto masyvo.
+// (Pvz., rastiDidziausia([3, 9, 2, 11, 7]) → 11.)
+
+let biggest = 0;
+const rastiDidziausia = [3, 9, 2, 11, 7];
+for (i = 0; i < rastiDidziausia.length; i++) {
+  if (rastiDidziausia[i] > biggest) {
+    biggest = rastiDidziausia[i];
+  }
+}
+
+console.log("     Trylikta");
+console.log(biggest);
+
+// 14. Sukurkite funkciją, kuri priima stringą ir grąžina jį be tarpų.
+// (Pvz., beTarpų("Labas pasauli") → "Labaspasauli".)
+
+function beTarpu(a) {
+  a = a.replaceAll(" ", "");
+
+  return a;
+}
+console.log(beTarpu("Labas pasauli"));
+
+// 15. Parašykite funkciją, kuri patikrina, ar string'e yra daugiau nei 10 simbolių.
+// (Pvz., arIlgas("JavaScript") → true, arIlgas("Labas") → false.)
+
+// 16. Sukurkite for ciklą, kuris išveda daugybos lentelę 5(nuo 1 iki 10).
+
+// 17. Parašykite funkciją, kuri priima stringą ir pakeičia visas "a" raides į "@".
+// (Pvz., keistiRaide("banana") → "b@n@n@".)
+
+// 18. Sukurkite funkciją, kuri sugeneruoja stringą "**********"(10 žvaigždučių), naudojant for ciklą.
+
+// 19. Parašykite funkciją, kuri priima skaičių ir grąžina visus jo daliklius(išskyrus 1 ir patį save).
+// (Pvz., dalikliai(12) →[2, 3, 4, 6].)
+
+// 20. Sukurkite funkciją, kuri tikrina, ar pateiktas skaičius yra pirminis.
+// (Pvz., arPirminis(7) → true, arPirminis(8) → false.)
--- js/intro/03-if.js
+++ js/intro/03-if.js
@@ -18,14 +18,304 @@ Loginiai operatoriai
 ||
 */
 
-console.log('Labas rytas');
-console.log('aaaaaa');
-
 if (4 >= 2) {
-    console.log('bbbbbb')
+  console.log("Taip");
 } else {
-    console.log('cccccc')
-
+  console.log("Ne");
 }
 
-console.log('dddddd')
+const a = 5;
+if (a === 1) {
+  console.log("vienas");
+} else if (a === 2) {
+  console.log("du");
+} else if (a === 3) {
+  console.log("trys");
+} else if (a === 4) {
+  console.log("keturi");
+} else if (a === 5) {
+  console.log("penki");
+} else if (a === 6) {
+  console.log("šeši");
+} else {
+  console.log("neatpažintas skaičius");
+}
+
+const n = 13;
+if (n % 2 === 0) {
+  console.log("lyginis");
+} else {
+  console.log("nelyginis");
+}
+
+switch (n % 2) {
+  case 0:
+    console.log("Lyginis");
+    break;
+  case 1:
+    console.log("Nelyginis");
+    break;
+}
+
+const d = 1;
+if (d === 1) {
+  console.log("pirmadienis");
+} else if (d === 2) {
+  console.log("t");
+} else if (d === 3) {
+  console.log("trečiadienis");
+} else if (d === 4) {
+  console.log("ketvirtadienis");
+} else if (d === 5) {
+  console.log("penktadienis");
+} else if (d === 6) {
+  console.log("sestadienis");
+} else if (d === 7) {
+  console.log("sekmadienis");
+}
+
+// nested if (kai folderio viduk kitas folderis)
+const d2 = 7;
+
+if (d2 === 1) {
+  console.log("pirmadienis");
+} else {
+  if (d2 === 2) {
+    console.log("antradienis");
+  } else {
+    if (d2 === 3) {
+      console.log("trečiadienis");
+    } else if (d2 === 4) {
+      console.log("ketvirtadienis");
+    } else {
+      if (d2 === 5) {
+        console.log("penktadienis");
+      } else {
+        if (d2 === 6) {
+          console.log("sestadienis");
+        } else {
+          if (d2 === 7) {
+            console.log("sekmadienis");
+          }
+        }
+      }
+    }
+  }
+}
+
+const d3 = 7;
+
+switch (d3) {
+  case 1:
+    console.log("pirmadienis");
+    break;
+  case 2:
+    console.log("antradienis");
+    break;
+  case 3:
+    console.log("trečiadienis");
+    break;
+  case 4:
+    console.log("Ketvirtadienis");
+    break;
+  case 5:
+    console.log("penktadienis");
+    break;
+  case 6:
+    console.log("sestadienis");
+    break;
+  case 7:
+    console.log("sekmadienis");
+    break;
+  default:
+    console.log("savaitėje nėra tokios dienos");
+}
+
+// 1-5 - darbo diena
+// 6-7 - savaitgalis
+
+const d4 = 4;
+
+if (d4 === 1 || d4 === 2 || d4 === 3 || d4 === 5) {
+  console.log("-- darbo diena");
+} else if (d4 === 6 || d4 === 7) {
+  console.log("-- savaitgalis");
+} else {
+  console.log("-- tokia diena neegzistuoja");
+}
+
+if (d4 >= 1 && d4 <= 5 && d4 % 1 === 0) {
+  console.log("## darbo diena");
+} else if (d4 === 6 || d4 === 7) {
+  console.log("## savaitgalis");
+} else {
+  console.log("## tokia diena neegzistuoja");
+}
+
+// switch  Case dirba iki kol sutinka break!!!
+
+const d5 = 6;
+switch (d5) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log("Darbo diena");
+    break;
+  case 6:
+  case 7:
+    console.log("Savaitgalis");
+    break;
+  default:
+    console.log("savaitėje nėra tokios dienos");
+}
+
+/*
+1. pasiimame puodeli
+2. į arbartinuką įpilam vandens
+3. užkaičiame
+4. į puodelį įdedame arbatos
+5. užpilame karštu vandeniu
+*/
+console.clear();
+
+const teaStep = 1;
+
+switch (teaStep) {
+  case 1:
+    console.log("pasiimame puodeli");
+  case 2:
+    console.log("į arbartinuką įpilam vandens");
+  case 3:
+    console.log("užkaičiame");
+  case 4:
+    console.log("į puodelį įdedame arbatos");
+  case 5:
+    console.log("užpilame karštu vandeniu");
+  default:
+    console.log("Labas");
+}
+
+const stop = "Rudens 2";
+
+switch (stop) {
+  case "Geniu 1":
+    console.log("🚌Geniu");
+  case "Tremtiniu 1":
+    console.log("🚌Tremtiniu");
+  case "Rudens 1":
+    console.log("🚌Rudens");
+  case "Mindaugo ":
+    console.log("🚌Mindaugo");
+  case "Zaliasis tiltas":
+    console.log("🚌Zaliasis tiltas");
+    break;
+  case "Lvovo":
+    console.log("🚌Lvovo");
+  case "Vasara":
+    console.log("🚌Vasara");
+  case "Rudens 2":
+    console.log("🚌Rudens");
+  case "Tremtiniu 2":
+    console.log("🚌Tremtiniu");
+  case "Geniu 2":
+    console.log("🚌Geniu");
+    break;
+}
+
+console.clear();
+
+const temp = 15;
+const isDay = false;
+if (temp > 18) {
+  switch (isDay) {
+    case true:
+      console.log("Silta");
+      break;
+    case false:
+      console.log("Silta naktis");
+      break;
+  }
+} else {
+  switch (isDay) {
+    case true:
+      console.log("Šalta diena");
+      break;
+    case false:
+      console.log("Šalta naktis");
+      break;
+  }
+}
+// UNARY (1)
+// TERNARY (2)
+// TERNARY (3)
+
+const x = 5;
+
+const y = 2 + 2;
+
+let text = "";
+if (4 > 2) {
+  text = "Taip";
+} else {
+  text = "Ne";
+}
+
+console.log(text);
+
+const text2 = 4 > 2 ? "Taip" : "Ne";
+console.log(text2);
+
+const amziausRiba = 18;
+const jonoAmzius = 99;
+const jonas = jonoAmzius >= amziausRiba ? "Pilnametis" : "nepilnametis";
+
+console.log(jonas);
+
+console.log(1 < 2 ? 3 : 4);
+console.log(2 > 3 ? 4 : 5 ? 6 : 7);
+console.log(1 < 2 ? (3 ? 4 : 5) : 6 ? 7 : 8);
+
+// visi skaičiai yra pozityvūs (truthy)
+// 0 yra negatyvus (falsy)
+
+if (0) {
+  console.log("ok");
+} else {
+  console.log("not so ok");
+}
+
+// visi string yra pozityvus
+// isskyrus tuscia
+
+if ("") {
+  console.log("ok");
+} else {
+  console.log("not ok");
+}
+
+// ! šauktukas apverčia logiką true False
+const username = "";
+
+if (!username) {
+  console.log("labas", username);
+} else {
+  console.log("Labas", username, "!");
+}
+
+console.log(0 ? (1 ? 2 : 3) : 4 ? 5 : 6);
+/*
+1 
+    ? 2 
+    : 3 
+
+    
+    0 
+        ? 1 
+            ? 2 
+            : 3 
+        : 4 
+            ? 5  - laimi
+            : 6
+*/
